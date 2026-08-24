ce6e7c506861651694fe0c02b823edd4f6977518
Airat Zaripov <omg.airat@gmail.com>
2026-08-24 15:53:17 +0300

    feat: implemented ProgressBar component

---
+++ src/components/UI/ProgressBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+  value: number;
+  max: number;
+  colorClass: string;
+}>();
+
+const computedStyle = computed(() => {
+  const percentage = props.value >= props.max ? 100 : (props.value / props.max) * 100;
+  return {
+    width: `${percentage}%`,
+  };
+});
+</script>
+
+<template>
+  <div class="w-full bg-dark-primary rounded-full overflow-hidden">
+    <div :class="['h-2', colorClass]" :style="computedStyle"></div>
+  </div>
+</template>
