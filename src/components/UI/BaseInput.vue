16eb3e5d2d447257631d8160532f0fe6de6c395f
Airat Zaripov <omg.airat@gmail.com>
2026-08-25 15:59:37 +0300

    feat: implement base input field

---
+++ src/components/UI/BaseInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    placeholder?: string;
+    type?: 'text' | 'number' | 'date';
+  }>(),
+  {
+    placeholder: '',
+    type: 'text',
+  },
+);
+
+const value = defineModel<string | number>();
+
+const handleFocus = ({ target }: FocusEvent) => {
+  if (target && props.type === 'date') {
+    (target as HTMLInputElement).showPicker();
+  }
+};
+</script>
+
+<template>
+  <input
+    v-model="value"
+    class="bg-dark-primary text-light placeholder:text-light-secondary focus:outline-none focus:border-mint-primary rounded-xl p-3"
+    :type="type"
+    :placeholder="placeholder"
+    @focus="handleFocus($event)"
+  />
+</template>
