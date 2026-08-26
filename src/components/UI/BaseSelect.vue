e97d2f8fcc551292c262c55217cf4ec9c59f4068
Airat Zaripov <omg.airat@gmail.com>
2026-08-26 15:58:33 +0300

    fix: change syntax of svg color in css

--- src/components/UI/BaseSelect.vue
+++ src/components/UI/BaseSelect.vue
@@ -28,13 +28,13 @@ const value = defineModel<string>();
 
 <style scoped>
 .select {
-  background-image: url('data:image/svg+xml;utf8,<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 9L12 15L18 9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke="rgb(167,167,167)"/></svg>');
+  background-image: url('data:image/svg+xml;utf8,<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 9L12 15L18 9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke="%23A7A7A7"/></svg>');
   background-repeat: no-repeat;
   background-position: right 10px center;
   background-size: 20px;
 }
 
 .select:focus {
-  background-image: url('data:image/svg+xml;utf8,<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 15L12 9L18 15" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke="rgb(167,167,167)"/></svg>');
+  background-image: url('data:image/svg+xml;utf8,<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 15L12 9L18 15" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke="%23A7A7A7"/></svg>');
 }
 </style>
