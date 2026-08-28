3379f7b39d2255a0cc3dee0e5a9d78093682e6b7
Airat Zaripov <omg.airat@gmail.com>
2026-08-28 16:10:44 +0300

    feat(state): enable localstorage persistence for transactions using pinia plugin

--- src/stores/finances.ts
+++ src/stores/finances.ts
@@ -3,37 +3,44 @@ import { computed, ref } from 'vue';
 import { TransactionTypes, type Transaction, type TransactionWithoutId } from '../types/finances';
 import { isCurrentMonth } from '../utils/date';
 
-export const useFinanceStore = defineStore('finances', () => {
-  const transactions = ref<Transaction[]>([]);
-  const categories = ref<string[]>(['Food', 'Transport', 'Salary', 'Utilities']); // Example categories
+export const useFinanceStore = defineStore(
+  'finances',
+  () => {
+    const transactions = ref<Transaction[]>([]);
+    const categories = ref<string[]>(['Food', 'Transport', 'Salary', 'Utilities']); // Example categories
 
-  const addTransaction = (transaction: TransactionWithoutId) => {
-    if (!Number.isFinite(Number(transaction.amount))) return;
+    const addTransaction = (transaction: TransactionWithoutId) => {
+      if (!Number.isFinite(Number(transaction.amount))) return;
 
-    transactions.value.push({ ...transaction, id: crypto.randomUUID(), amount: Number(transaction.amount) });
-  };
+      transactions.value.push({ ...transaction, id: crypto.randomUUID(), amount: Number(transaction.amount) });
+    };
 
-  const monthIncome = computed<number>(() => {
-    return transactions.value
-      .filter((transaction) => transaction.type === TransactionTypes.INCOME && isCurrentMonth(transaction.date))
-      .reduce((total, transaction) => total + transaction.amount, 0);
-  });
+    const monthIncome = computed<number>(() => {
+      return transactions.value
+        .filter((transaction) => transaction.type === TransactionTypes.INCOME && isCurrentMonth(transaction.date))
+        .reduce((total, transaction) => total + transaction.amount, 0);
+    });
 
-  const monthSpending = computed<number>(() => {
-    return transactions.value
-      .filter((transaction) => transaction.type === TransactionTypes.SPENDING && isCurrentMonth(transaction.date))
-      .reduce((total, transaction) => total + transaction.amount, 0);
-  });
+    const monthSpending = computed<number>(() => {
+      return transactions.value
+        .filter((transaction) => transaction.type === TransactionTypes.SPENDING && isCurrentMonth(transaction.date))
+        .reduce((total, transaction) => total + transaction.amount, 0);
+    });
 
-  const transactionsReversed = computed<Transaction[]>(() => {
-    return [...transactions.value].reverse();
-  });
+    const transactionsReversed = computed<Transaction[]>(() => {
+      return [...transactions.value].reverse();
+    });
 
-  return {
-    categories,
-    monthIncome,
-    monthSpending,
-    addTransaction,
-    transactionsReversed,
-  };
-});
+    return {
+      categories,
+      transactions,
+      monthIncome,
+      monthSpending,
+      addTransaction,
+      transactionsReversed,
+    };
+  },
+  {
+    persist: true,
+  },
+);
