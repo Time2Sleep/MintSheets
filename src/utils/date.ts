618651f8723679c5fcd5153813de616f4aa52308
Airat Zaripov <omg.airat@gmail.com>
2026-09-09 16:51:19 +0300

    fix: update date handling in transaction data mapping and enhance date utility functions

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,10 +1,13 @@
-export const getTodayDateFormatted = (): string => {
-  const date = new Date();
+export const getDateFormatted = (date: Date): string => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
 
   return `${year}-${month}-${day}`;
+};
+
+export const getTodayDateFormatted = (): string => {
+  return getDateFormatted(new Date());
 };
 
 export const isCurrentMonth = (dateString: string): boolean => {
@@ -32,4 +35,25 @@ export const dateToHumanReadable = (dateString: string): string => {
   if (date.getFullYear() !== today.getFullYear()) year = date.getFullYear().toString();
 
   return `${date.getDate()} ${date.toLocaleString('en-US', { month: 'long' })} ${year}`;
+};
+
+export const stringDateToSheetDate = (dateString: string): number => {
+  const date = new Date(dateString);
+  const msPerDay = 24 * 60 * 60 * 1000;
+  const excelBaseDate = new Date('1899-12-30'); // Google sheets reference date
+
+  return (date.getTime() - excelBaseDate.getTime()) / msPerDay;
+};
+
+export const sheetDateToStringDate = (sheetDate: string | number): string => {
+  if (typeof sheetDate === 'string') return sheetDate;
+
+  const msPerDay = 24 * 60 * 60 * 1000;
+  const daysBetween = sheetDate - 25569;
+
+  const date = new Date(daysBetween * msPerDay);
+  const timezoneOffset = date.getTimezoneOffset() * 60 * 1000;
+  const dateNormalized = new Date(date.getTime() + timezoneOffset);
+
+  return getDateFormatted(dateNormalized);
 };
