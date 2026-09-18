9271a8482b21fc60f0749640e169761bac52ac3b
Airat Zaripov <omg.airat@gmail.com>
2026-09-18 19:39:05 +0300

    chore: pass to offline mode only if spreadsheetId is set

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,9 +12,11 @@ router.beforeEach((to) => {
   const googleStore = useGoogleStore();
   const isAuthRequired = to.meta.requiresAuth;
   const isAuthenticated = !!googleStore.googleToken;
-  const { isOffline, mintsWasConnected } = storeToRefs(googleStore);
+  const { isOffline, mintsWasConnected, spreadsheetId } = storeToRefs(googleStore);
 
-  if (isAuthRequired && !isAuthenticated && (!isOffline.value || !mintsWasConnected.value)) {
+  const offline = spreadsheetId && isOffline.value && mintsWasConnected.value;
+
+  if (isAuthRequired && !isAuthenticated && !offline) {
     return { name: 'auth' };
   } else if (to.name === 'auth' && isAuthenticated) {
     return { name: 'main' };
